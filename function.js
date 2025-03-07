6fbbf0e30fc04ee1b2cd5f72e44cbf3e766548d4
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-03-07 14:18:01 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,15 @@
 // Ottieni token per autenticazione
-const getWikiAuthToken = async (dominio) => {   // dominio dell'istanza Wiki da interrogare, può essere 'www.wikidata.org' oppure stanze wikibase come 'vicobncf.wikibase.cloud'
+const getWikiAuthToken = async (endpoint) => {   // dominio dell'istanza Wiki da interrogare, può essere 'https://www.wikidata.org/w/api.php' oppure stanze wikibase come 'https://vicobncf.wikibase.cloud/w/api.php'
 
-    const url = `https://${dominio}/w/api.php?action=query&meta=tokens&format=json`; // API Endpoint per l'ottenimento del token di autenticazione
-    const response = await fetch(url, { credentials: "include" });  // Esecuzione della cihamata
+    // Parametri URL
+    const params = new URLSearchParams({
+        action: "query",
+        meta: "token",
+        format: "json"
+    });
+
+    const url = `${endpoint}?${params.toString()}`; // API Endpoint per l'ottenimento del token di autenticazione
+    const response = await fetch(url, { credentials: "include" });  // Esecuzione della chiamata
     const json = await response.json();  // Conversione della risposta
     const token = json.query.tokens.csrftoken  // Estrazione valore token di autenticazione
 
